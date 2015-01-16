bcbd3f38512afd2afdfaab9e10d8811f8fa0b241
chikeichan <chikeichan@gmail.com>
2015-01-16 15:21:25 -0800

    Done with karma test

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,18 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, $http, Links) {
   // Your code here
+  $scope.data = {
+    links: []
+  };
+
+  $scope.getLinks = function(){
+    $http.get('/api/links')
+      .success(function(data,status,headers,config){
+        $scope.data.links = $scope.data.links.concat(data);
+        return data;
+      })
+  };
+
+  $scope.getLinks();
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,15 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, $http, Links) {
   // Your code here
+  $scope.link = {};
+  $scope.addLink = function(link){
+    var url = $location.path();
+    // console.log('URL:',url);
+    // console.log('LINK:',link);
+    $http.post('/api/links', {url:link})
+      .success(function(data,status,headers,config){
+        // console.log(data,status);
+      })
+  };
 });
